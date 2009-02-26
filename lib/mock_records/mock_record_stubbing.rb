4eade3ff9c8dad0bc9d0d77f65d7bbdbd8215d01
Peter Jaros <peeja@dropio.com>
2009-02-26 13:58:44 -0500

    Refactor #stub_new_records to use MockRecord's new features.

--- lib/mock_records/mock_record_stubbing.rb
+++ lib/mock_records/mock_record_stubbing.rb
@@ -4,23 +4,19 @@ module MockRecords
   module MockRecordStubbing
     def stub_new_records(klass)
       mock_records = Array.new
-    
-      make_new_record = lambda do |attrs, save|
-        mr = MockRecord.new
-        attrs.each { |k, v| mr.send "#{k}=", v }
-        mr.save! if save
+      
+      make_new_record = lambda do |method, attrs|
+        mr = MockRecord.send(method, attrs)
         mock_records << mr
         mr
       end
-    
+      
       # Stub creation methods
-      [[:new,     false],
-       [:create,  true],
-       [:create!, true]].each do |method, save|
-         klass.stub!(method).with().and_return { make_new_record[{}, save] }
-         klass.stub!(method).with(an_instance_of(Hash)).and_return { |attrs| make_new_record[attrs, save] }
+      [:new, :create, :create!].each do |method|
+         klass.stub!(method).with().and_return { make_new_record[method, {}] }
+         klass.stub!(method).with(an_instance_of(Hash)).and_return { |attrs| make_new_record[method, attrs] }
       end
-    
+      
       klass.stub!(:new_records).and_return do
         mock_records.select { |mr| mr.saved? }.each { |mr| mr.reload }
       end
