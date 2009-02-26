9b85bd2a1f41125c9b4f42076a399836bf6d56bc
Peter Jaros <peeja@dropio.com>
2009-02-26 11:20:11 -0500

    Refactor creation methods.

--- lib/mock_record_stubbing.rb
+++ lib/mock_record_stubbing.rb
@@ -5,47 +5,20 @@ module MockRecordStubbing
   def stub_new_records(klass)
     mock_records = Array.new
     
-    klass.stub!(:new).with().and_return do
-      mr = MockRecord.new
-      mock_records << mr
-      mr
-    end
-    
-    klass.stub!(:new).with(an_instance_of(Hash)).and_return do |attrs|
+    make_new_record = lambda do |attrs, save|
       mr = MockRecord.new
       attrs.each { |k, v| mr.send "#{k}=", v }
+      mr.save! if save
       mock_records << mr
       mr
     end
     
-    klass.stub!(:create).with().and_return do
-      mr = MockRecord.new
-      mr.save!
-      mock_records << mr
-      mr
-    end
-    
-    klass.stub!(:create).with(an_instance_of(Hash)).and_return do |attrs|
-      mr = MockRecord.new
-      attrs.each { |k, v| mr.send "#{k}=", v }
-      mr.save!
-      mock_records << mr
-      mr
-    end
-    
-    klass.stub!(:create!).with().and_return do
-      mr = MockRecord.new
-      mr.save!
-      mock_records << mr
-      mr
-    end
-    
-    klass.stub!(:create!).with(an_instance_of(Hash)).and_return do |attrs|
-      mr = MockRecord.new
-      attrs.each { |k, v| mr.send "#{k}=", v }
-      mr.save!
-      mock_records << mr
-      mr
+    # Stub creation methods
+    [[:new,     false],
+     [:create,  true],
+     [:create!, true]].each do |method, save|
+       klass.stub!(method).with().and_return { make_new_record[{}, save] }
+       klass.stub!(method).with(an_instance_of(Hash)).and_return { |attrs| make_new_record[attrs, save] }
     end
     
     klass.stub!(:mock_records).and_return(mock_records)
